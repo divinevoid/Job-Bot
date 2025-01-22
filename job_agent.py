7de6318025a0f6374a0d9e0355823e7c0facc280
divinevoid <jatinjoshi921@hmail.com>
2025-01-22 15:52:37 +0530

    Required st

--- job_agent.py
+++ job_agent.py
@@ -18,6 +18,12 @@ import streamlit as st
 from datetime import datetime
 import tempfile
 import os
+import pysqlite3 as sqlite3
+
+__import__("pysqlite3")
+import sys
+
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 load_dotenv()
 
